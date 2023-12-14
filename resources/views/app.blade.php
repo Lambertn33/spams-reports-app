d4fd0f87a3028b25995d24e3530b2921a39e405f
Lambertn33 <lambertgithub@gmail.com>
2023-12-14 07:59:04 +0200

    feat: setup inertia in blade

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+
+<style>
+    body {
+        font-family: 'Poppins', sans-serif;
+    }
+</style>
+
+<body>
+    @inertia
+</body>
+
+</html>

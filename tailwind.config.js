e923b206ce1ce94e4c400bcbb4fb57c9df2420c1
Lambertn33 <lambertgithub@gmail.com>
2023-12-14 07:53:42 +0200

    feat: tailwind installation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "./storage/framework/views/*.php",
+      "./resources/views/**/*.blade.php",
+      "./resources/js/**/*.vue",
+      "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}",
+      "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};

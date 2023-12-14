e94a5deb0f279d18115350039f3c09350163578d
Lambertn33 <lambertgithub@gmail.com>
2023-12-14 08:26:52 +0200

    feat: render protectors view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProtectorsSpamReportsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('app');
-});
+// Route::get('/', function () {
+//     return view('app');
+// });
+Route::resource('protectors/reports', ProtectorsSpamReportsController::class);
